f3f35596935a2756d4b8e00b864dacc07da37b15
mian11umar22 <mian11umar22@gmail.com>
2026-03-06 17:13:08 +0500

    SEO change

--- pharmacy-next/app/layout.js
+++ pharmacy-next/app/layout.js
@@ -47,6 +47,47 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} ${outfit.variable} font-sans antialiased text-secondary`}>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Pharmacy",
+              "name": "Hope Pharmacy",
+              "image": "https://www.hopepharmacypk.com/images/logo.png",
+              "@id": "https://www.hopepharmacypk.com",
+              "url": "https://www.hopepharmacypk.com",
+              "telephone": "0305-4964343",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Abid road Islam nagar main Walton",
+                "addressLocality": "Lahore",
+                "addressRegion": "Punjab",
+                "postalCode": "54000",
+                "addressCountry": "PK"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": 31.4805,
+                "longitude": 74.3718
+              },
+              "openingHoursSpecification": {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                  "Monday",
+                  "Tuesday",
+                  "Wednesday",
+                  "Thursday",
+                  "Friday",
+                  "Saturday",
+                  "Sunday"
+                ],
+                "opens": "00:00",
+                "closes": "23:59"
+              }
+            })
+          }}
+        />
         <AuthProvider>
           <CartProvider>
             {children}
